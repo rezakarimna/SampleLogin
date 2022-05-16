6764f592881749bdb97eb704dd0b39f18684fb5e
Reza <karimnia.reza70@yahoo.com>
2022-05-16 19:13:38 +0430

    create ui login

--- app/src/main/java/com/reza/samplelogin/LoginFragment.kt
+++ app/src/main/java/com/reza/samplelogin/LoginFragment.kt
@@ -9,7 +9,6 @@ import android.view.ViewGroup
 
 class LoginFragment : Fragment() {
 
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
