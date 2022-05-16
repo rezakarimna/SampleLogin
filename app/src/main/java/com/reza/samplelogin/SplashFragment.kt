75dd5c49a863aecdaaf9f98658c87a9321828803
Reza <karimnia.reza70@yahoo.com>
2022-05-16 16:25:06 +0430

    set ui splash screen

--- app/src/main/java/com/reza/samplelogin/SplashFragment.kt
+++ app/src/main/java/com/reza/samplelogin/SplashFragment.kt
@@ -5,15 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.reza.samplelogin.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment() {
-
+    lateinit var binding: FragmentSplashBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        // Inflate the layout for this
+        binding = FragmentSplashBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
 }
