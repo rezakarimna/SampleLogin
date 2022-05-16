5a011169a33edb1c4705cfcb4efb247ee596e380
Reza <karimnia.reza70@yahoo.com>
2022-05-16 16:34:04 +0430

    set navigation in MainActivity

--- app/src/main/java/com/reza/samplelogin/MainActivity.kt
+++ app/src/main/java/com/reza/samplelogin/MainActivity.kt
@@ -2,10 +2,27 @@ package com.reza.samplelogin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+
+import com.reza.samplelogin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var navController: NavController
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setNavController()
+    }
+
+    private fun setNavController() {
+        binding.apply {
+            navController = findNavController(R.id.navHost)
+        }
     }
 }
